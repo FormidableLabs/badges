20d0861645d7f0ebe0b718c56e44451322dfab5a
Jack Ross <jack-ross-iii@pm.me>
2022-05-06 14:05:56 -0400

    [#76] Moving from CodeBuild to GitHub Actions
    * First draft seems to be working for both pull requests and branch
      pushes, through the entire pipeline. Simulated deploying to
      production (by using a development tier) and from a non-master branch
    * No longer relying on manual approvals in CodePipeline to push to
      production
    * Now deploying to production on pushes to the master branch
    * Updated the ACM certificate being used, was previously pointing to a
      certificate that did not exist
    * Updated the number of pull requests that we're pulling using Octokit
      from 30 (the default) to 100 (the maximum)
    * Posting a comment from GitHub Actions rather than through CodeBuild as
      was being done previously

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,16 @@
-. ${CODEBUILD_SRC_DIR}/scripts/install_volta.sh
+. ${GITHUB_WORKSPACE}/scripts/install_volta.sh
 
 # Install Volta using functions imported above.
 check_architecture "0.6.3" "$(uname -m)" || exit 1
-install_version "0.6.3" "/root/.volta"
+install_version "0.6.3" "/tmp/.volta"
+# Reloading bashrc for volta
+source ~/.bashrc
 
 # Install tfenv to install the correct Terraform version.
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 PATH="$HOME/.tfenv/bin:$PATH" ~/.tfenv/bin/tfenv install
+# Terraform is already installed
+rm /usr/local/bin/terraform
 ln -s ~/.tfenv/bin/* /usr/local/bin
 
 # Install Terragrunt.
@@ -14,11 +18,11 @@ curl -L -o /usr/local/bin/terragrunt https://github.com/gruntwork-io/terragrunt/
 chmod +x /usr/local/bin/terragrunt
 
 # Use Volta to install pinned versions of Node and Yarn.
-/root/.volta/volta install node
-/root/.volta/volta install yarn
+/tmp/.volta/volta install node
+/tmp/.volta/volta install yarn
 
 # Install Yarn packages.
-/root/.volta/bin/yarn --frozen-lockfile
+/tmp/.volta/bin/yarn --frozen-lockfile
 
 if [[ ! -z "${TERRAFORM_AWS_SERVERLESS_BRANCH}" ]]; then
   git clone -v --single-branch --branch "${TERRAFORM_AWS_SERVERLESS_BRANCH}" https://github.com/FormidableLabs/terraform-aws-serverless ../terraform-aws-serverless
